ca7ea71ddc2badefc2dabd2485f6811cfb762052
Ross <rosshamey@gmail.com>
2023-04-30 20:50:47 -0400

    Create printNums.java

---
+++ printNums.java
@@ -0,0 +1,9 @@
+class myClass {
+    public static void tenToOne() {
+        int value = 10;
+        while (value > 0) {
+            System.out.println(value);
+            value = value - 1;
+        }
+    }
+}
