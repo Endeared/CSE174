92e4dfccfd2c21c89855bfaacfa2d82e6b00d823
Ross <rosshamey@gmail.com>
2023-05-14 12:38:05 -0400

    root

--- Testing.java
+++ Testing.java
@@ -2,9 +2,8 @@ public class Testing {
   public static void main(String[] args) {
    
     double base = 3.0;
-    double height = 4.0;
-    double area = base * height / 2;
-    System.out.println(area);
+    double root = Math.sqrt(base);
+    System.out.println(root);
   
   }
 }
