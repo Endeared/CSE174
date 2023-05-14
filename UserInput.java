9ae83f168fde552ab852e627664cc9f11d51748b
Ross <rosshamey@gmail.com>
2023-05-14 12:50:31 -0400

    change scanner name

--- UserInput.java
+++ UserInput.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class UserInput {
   public static void main(String[] args) {
     
-    Scanner keyboardReader = new Scanner(System.in);
+    Scanner keyboard = new Scanner(System.in);
     
     System.out.println("Please input the first number.");
-    double firstNum = keyboardReader.nextDouble();
+    double firstNum = keyboard.nextDouble();
     System.out.println("Please input the second number.");
-    double secondNum = keyboardReader.nextDouble();
+    double secondNum = keyboard.nextDouble();
     
     double sum = firstNum + secondNum;
     System.out.printf("The sum of your two numbers is %s.%n", sum);
