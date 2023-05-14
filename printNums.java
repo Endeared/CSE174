eaac41e08babe1be1f324a5633ecc7366895df16
Ross <rosshamey@gmail.com>
2023-05-14 10:40:09 -0400

    remove others

--- printNums.java
+++
@@ -1,9 +0,0 @@
-class myClass {
-    public static void tenToOne() {
-        int value = 10;
-        while (value > 0) {
-            System.out.println(value);
-            value = value - 1;
-        }
-    }
-}
