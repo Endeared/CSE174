cff67585e62826da7bfd4cd59b50c4352ec1df37
Ross <rosshamey@gmail.com>
2023-05-14 12:58:18 -0400

    reading input / files

--- UserInput.java
+++ UserInput.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class UserInput {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     
     Scanner keyboard = new Scanner(System.in);
+    Scanner file = new Scanner(new File("data.txt"));
     
     System.out.println("Please input the first number.");
     double firstNum = keyboard.nextDouble();
@@ -12,5 +15,11 @@ public class UserInput {
     
     double sum = firstNum + secondNum;
     System.out.printf("The sum of your two numbers is %s.%n", sum);
+    
+    int studentNum = file.nextInt();
+    String firstName = file.next();
+    String lastName = file.next();
+    String className = file.next();
+    System.out.printf("Your student number is " + studentNum + ". You are registered as " + firstName + " " + lastName + " under the class " + className + ".%n");
   }
 }
