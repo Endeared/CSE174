ec42c04b99f443726f333880634c55bcb9c57e4e
Ross <rosshamey@gmail.com>
2023-05-14 12:35:39 -0400

    adding style checks

---
+++ Testing.java
@@ -0,0 +1,10 @@
+public class Testing {
+  public static void main(String[] args) {
+   
+    double base = 3.0;
+    double height = 4.0;
+    double area = base * height / 2;
+    System.out.println(area);
+  
+  }
+}
