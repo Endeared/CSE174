024f4236a860dd352a63bc1d97981281e521c123
Ross <rosshamey@gmail.com>
2023-05-14 12:43:26 -0400

    %n alignment

--- Testing.java
+++ Testing.java
@@ -3,7 +3,15 @@ public class Testing {
    
     double base = 3.0;
     double root = Math.sqrt(base);
-    System.out.printf("With a base of %.2f, the root of said base is %.4f.", base, root);
-  
+    
+    double testOne = 1.0;
+    double testTwo = 5.44;
+    double testThree = 14.447;
+    
+    System.out.printf("With a base of %.2f, the root of said base is %.4f.%n", base, root);
+    System.out.printf("%6.2f%n", testOne);
+    System.out.printf("%6.2f%n", testTwo);
+    System.out.printf("%6.2f%n", testThree);
+    
   }
 }
