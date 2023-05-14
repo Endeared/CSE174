9da444048701844e0e74ee84e30809ff8dccfe6a
Ross <rosshamey@gmail.com>
2023-05-14 11:22:16 -0400

    primitive data types

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,15 @@
+public class PrimitiveDataTypes {
+   public static void main(String[] args) {
+   
+      int numberOfSides;
+      double lengthOfSide;
+      double perimeter;
+      
+      numberOfSides = 5;
+      lengthOfSide = 1.3;
+      perimeter = numberOfSides * lengthOfSide;
+      
+      System.out.println(perimeter);
+      
+   }
+}
