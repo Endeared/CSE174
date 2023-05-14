3265be216327d00a333fd2d9ba9c2441ebfd3ea8
Ross <rosshamey@gmail.com>
2023-05-14 11:13:29 -0400

    mistakeswemake class

---
+++ MistakesWeMake.java
@@ -0,0 +1,8 @@
+public class MistakesWeMake {
+   public static void main(String[] args) {
+      int x = 15;
+      int y = 2;
+      int z = x / y;
+      System.out.println("The answer is: " + z);
+   }
+}
