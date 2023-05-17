aca2738b0ca3b04d71ce79dcfd1d84ae495cd6ef
Ross <rosshamey@gmail.com>
2023-05-17 08:09:55 -0400

    coffeecosts

--- AreaChecker.java
+++ AreaChecker.java
@@ -4,29 +4,30 @@
  * Computes the area of a trapazoid using two sets of values.
  */
 public class AreaChecker {
-  public static void main(String[] args){
+  public static void main(String[] args) {
+    
+    double longBase = 10;
+    double shortBase = 5;
+    double altitude = longBase * shortBase;
+    double area = 0.0;
         
-    double longBase=10;
-    double shortBase=5;
-    double altitude=longBase*shortBase;
-    double area =0.0;
-        
-    // \n is called an escape code and it moves the curser
-    // to the next line. So whatever that is printed after
-    // that, will be printed on the new/next line.
-    System.out.println("Long Base ="+ longBase + "\nShort Base =" + shortBase);
+    System.out.println("Long Base = " + longBase);
+    System.out.println("Short Base = " + shortBase);
     System.out.println("Altitude = " + altitude);
-        
-    System.out.println("adjusting altitude to 9:");
+    
+    /* CODE expected the ":" as part of the string on
+    * canvas so i left it in, despite no output following
+    * it...
+    */
+    System.out.println("\nAdjusting Altitude to 9:");
     altitude = 9;
         
-    System.out.println("Long Base =" +longBase);
-      System.out.println("Short Base =" + shortBase);
-    System.out.println("Altitude = "+altitude);
-        
-    System.out.print("Area = ");
-    area = (longBase +shortBase) / 2 *altitude;
-    System.out.println (area);        
-        
-   }
+    System.out.println("Long Base = " + longBase);
+    System.out.println("Short Base = " + shortBase);
+    System.out.println("Altitude = " + altitude);
+    
+    area = (longBase + shortBase) / 2 * altitude;
+    System.out.println("\nArea = " + area);   
+    
+  }
 }
---
+++ CoffeeCosts.java
@@ -0,0 +1,11 @@
+/*  cse174 lab1pt2 - calculating coffee costs
+*   Ross Hamey
+*   5/17/2023
+*/
+public class CoffeeCosts {
+  public static void main(String[] args) {
+  
+    
+    
+  }
+}
