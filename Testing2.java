95b5c3469be9a7b3e5bf50a5dcdc970847b92f0c
Ross <rosshamey@gmail.com>
2023-05-23 17:18:33 -0400

    testing class

---
+++ Testing2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Testing2 {
+  public static void main(String[] args) {
+      
+    int i = 0;
+    int j = 0;
+    while (i < 27) {
+       i = i + 2;
+       j++;
+    }
+    System.out.println("j=" + j);
+    
+  }
+}
