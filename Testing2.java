16e9526618a373e3b920bdb4bc5bd23431a32212
Ross <rosshamey@gmail.com>
2023-05-24 12:11:08 -0400

    last lab before trip

--- Testing2.java
+++ Testing2.java
@@ -3,18 +3,14 @@ import java.util.Scanner;
 public class Testing2 {
   public static void main(String[] args) {
           
-    int years = 50;
-    double balance = 10000;
-    double targetBalance = 20000;
-    double rate = 3;
-    for (int i = 1; i <= years; i++) {
-       if (balance >= targetBalance) {
-          i = years + 1;
-       } else {
-          double interest = balance * rate / 100;
-          balance = balance + interest;
-       }
-       System.out.println("executed");
+    int x = 1;
+    while (x <= 6) {
+        for (int i = 0; i == 0; i++) {
+            System.out.print("*");        //
+    
+    
+        }
+        x *= 2;
     }
 
   }
