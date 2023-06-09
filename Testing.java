a06442e9cc58da185309821a41c139de32883a4d
Ross <rosshamey@gmail.com>
2023-06-09 15:49:40 -0400

    test

--- Testing.java
+++ Testing.java
@@ -1,30 +1,22 @@
-import java.util.*;
+import java.awt.Point;
 
-public class Testing {
+public class Testing { 
   public static void main(String[] args) {
-   
-    Scanner in = new Scanner(System.in);
-    int x = in.nextInt();
-    int y = in.nextInt();
+  
+    Point a = new Point(3, 4);
+    Point b = null;
     
-    if ( x < 0 ) {
-        if ( y < 0 ) {
-            System.out.println("A");
-            System.out.println("B");
-        } else if ( y == 0 ) {
-            System.out.println("C");
-        } else {
-            System.out.println("D");
-        }
-    } else if ( x == 0 ) {
-        System.out.println("E");
-    } else {
-        if ( y % 2 == 0 ) {
-            System.out.println("F");
-        } else {
-            System.out.println("G");
-        }
-    }
+
+    System.out.println(a);
+    
+    System.out.println(a.equals(b));
+    
+    System.out.println(a == b);
+    
+    System.out.println(b);
+    
+    boolean result = (b == b);
     
   }
+
 }
